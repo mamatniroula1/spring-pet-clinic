433d90bf8abb1024aeb5fc393e73e843aa382f97
sunilniroula1 <sunilniroula1@gmail.com>
2020-04-08 17:46:08 -0500

    list of owners shown in the index page

--- com.sunil-pet-clinic-web/src/main/java/com/sunil/springpetclinic/controllers/OwnerController.java
+++ com.sunil-pet-clinic-web/src/main/java/com/sunil/springpetclinic/controllers/OwnerController.java
@@ -1,14 +1,23 @@
 package com.sunil.springpetclinic.controllers;
 
+import com.sunil.springpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"/","","/index","/index.html"})
-    public String ListOwners(){
+    public String ListOwners(Model model){
+       model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 }
